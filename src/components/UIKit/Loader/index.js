ea3537c88945a143df2ddd9e22cdea8d85b3b3a4
omkarvaigankar8 <omkar@tentwenty.me>
2022-07-18 00:55:51 +0530

    minor fix

--- src/components/UIKit/Loader/index.js
+++ src/components/UIKit/Loader/index.js
@@ -4,7 +4,6 @@ import ClipLoader from "react-spinners/RingLoader";
 function Loader({loading}) {
 
   let color = "#ffffff";
-    console.log("load",loading)
   return (
     <div className="sweet-loading">
       <ClipLoader color={color} loading={loading}   size={150} />
